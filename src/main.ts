08d837ec8563be28327cdc1ec26fcec09ea16cde
Александр Варламов <odoleev@gmail.com>
2023-01-20 21:36:54 +0300

    cors2

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.enableCors({
+  app.enableCors({
     allowedHeaders: '*',
     origin: '*',
+    credentials: true
   });
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
