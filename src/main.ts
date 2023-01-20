68da6ba1c105abd19a2ad71b359666e3ee3582bb
Александр Варламов <odoleev@gmail.com>
2023-01-20 21:24:03 +0300

    chaged cors policy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://user-table-front.vercel.app'],
+  });
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
