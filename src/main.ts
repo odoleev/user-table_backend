49b3ce6a5628067296fbbbfc4e7ca90aad7b2c5b
Александр Варламов <odoleev@gmail.com>
2023-01-20 20:26:08 +0300

    one more

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
