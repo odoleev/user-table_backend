a910126d7f9775620eef4145d682a848479c8e36
Александр Варламов <odoleev@gmail.com>
2023-01-20 21:33:32 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
+  await app.enableCors({
     allowedHeaders: '*',
     origin: '*',
   });
