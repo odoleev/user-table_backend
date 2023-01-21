8d00447418d23512eb5461ec51550abd708ddd5f
Александр Варламов <odoleev@gmail.com>
2023-01-21 08:32:50 +0300

    cors direct

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function start() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: ['https://user-table-front.vercel.app'],
   });
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
