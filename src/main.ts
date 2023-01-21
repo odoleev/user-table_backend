82be42d3384bf29a96dba1df47e23f53bf05a0d9
Александр Варламов <odoleev@gmail.com>
2023-01-21 08:30:50 +0300

    cors policy roll back

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+  });
+
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 
