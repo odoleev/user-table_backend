6b582b6e2da8ab0642509fe390131c9bd711239b
Александр Варламов <odoleev@gmail.com>
2023-01-21 08:47:50 +0300

    cors origin localhost try

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function start() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://user-table-front.vercel.app'],
+    origin: ['https://user-table-front.vercel.app', 'http://localhost:5173'],
   });
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
